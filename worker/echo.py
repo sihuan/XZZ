fefb0ced5497dc56e45b51ad1937bc43e5ced205
SiHuan <sihuan@sakuya.love>
2020-02-29 00:11:41 +0800

    echo up

--- worker/echo.py
+++ worker/echo.py
@@ -3,6 +3,8 @@ from zzcore import StdAns
 
 class Ans(StdAns):
     def GETMSG(self):
+        if len(self.parms) < 2:
+            return '不加参数是坏文明！'
         r = random.randint(1,3)
         if r == 1:
             msg = self.raw_msg['message'][5:]
