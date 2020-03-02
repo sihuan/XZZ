80eb4956944f282188c60f7f99b7f8e1e33f2f6d
Si-Huan <49349784+Si-Huan@users.noreply.github.com>
2020-03-02 21:54:54 +0800

    ns up up up up

--- worker/ns.py
+++ worker/ns.py
@@ -4,5 +4,6 @@ from subprocess import run
 class Ans(StdAns):
     def GETMSG(self):
         domain = self.raw_msg['message'][4:]
-        msg = run(["nslookup",domain],capture_output=True)
+        r = run(["nslookup",domain],capture_output=True)
+        msg = r.stdout
         return msg
