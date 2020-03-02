c7bb7228daa0d770b731f1bf69ae2af8e44f1bdc
Si-Huan <49349784+Si-Huan@users.noreply.github.com>
2020-03-02 21:44:10 +0800

    ns up up

--- worker/ns.py
+++ worker/ns.py
@@ -4,6 +4,6 @@ from subprocess import Popen
 class Ans(StdAns):
     def GETMSG(self):
         domain = self.raw_msg['message'][4:]
-        p = Popen(["nslookup ",domain])
+        p = Popen(["nslookup",domain])
         msg = p.read()
         return msg
