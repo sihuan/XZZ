a98cc9f8d67fa4a29343864aefa14feaccf36ac1
Si-Huan <49349784+Si-Huan@users.noreply.github.com>
2020-03-02 21:58:51 +0800

    ns up up up up

--- worker/ns.py
+++ worker/ns.py
@@ -5,5 +5,5 @@ class Ans(StdAns):
     def GETMSG(self):
         domain = self.raw_msg['message'][4:]
         r = run(["nslookup",domain],capture_output=True)
-        msg = r.stdout
+        msg = bytes.decode(r.stdout)
         return msg
