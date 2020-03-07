076eaab533341e542de7220be24beb69746ee5d1
weilinfox <weilin.king@qq.com>
2020-03-07 17:36:52 +0800

    fix bc bug

--- worker/ns.py
+++ worker/ns.py
@@ -1,18 +1,15 @@
 from zzcore import StdAns
-import subprocess
+from subprocess import check_output
 
 class Ans(StdAns):
     def GETMSG(self):
+        if len(self.parms) < 2:
+            return '不加参数是坏文明！'
+
+        domain = self.raw_msg['message'][4:]
         try:
-            if (len(self.parms) < 2):
-                return "不加参数是坏文明！"
-            com = ""
-            for s in self.parms:
-                com += s + ' '
-            com = 'nslookup \"' + com[3:len(com)-1] + '\"'
-            s = subprocess.run(com, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, timeout = 5)
-            return bytes.decode(s.stdout)
-        except subprocess.TimeoutExpired:
-            return "请求超时！"
+            answer = check_output(['nslookup',domain],shell=False,timeout=4)
+            msg = bytes.decode(answer)
         except:
-            return "未知错误！"
+            msg = '汝干了什么！ ns 超时了！'
+        return msg
