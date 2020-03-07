f003fa226578cedaeb81f97a401920a7619c3521
weilinfox <weilin.king@qq.com>
2020-03-07 11:59:19 +0800

    fix ns

--- worker/ns.py
+++ worker/ns.py
@@ -1,9 +1,18 @@
 from zzcore import StdAns
-from subprocess import run
+import subprocess
 
 class Ans(StdAns):
     def GETMSG(self):
-        domain = self.raw_msg['message'][4:]
-        r = run(["nslookup",domain],capture_output=True)
-        msg = bytes.decode(r.stdout)
-        return msg
+        try:
+            if (len(self.parms) < 2):
+                return "不加参数是坏文明！"
+            com = ""
+            for s in self.parms:
+                com += s + ' '
+            com = 'nslookup \"' + com[3:len(com)-1] + '\"'
+            s = subprocess.run(com, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, timeout = 5)
+            return bytes.decode(s.stdout)
+        except subprocess.TimeoutExpired:
+            return "请求超时！"
+        except:
+            return "未知错误！"
