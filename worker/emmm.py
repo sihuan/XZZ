3a6f81375d9268dc019ea80944bda700a12d99cf
SiHuan <sihuan@sakuya.love>
2020-06-18 19:25:32 +0800

    update emmm

--- worker/emmm.py
+++ worker/emmm.py
@@ -1,8 +1,15 @@
 from zzcore import StdAns
 
+
 class Ans(StdAns):
+    NotAllowGroup = [204097403]
+
+    def CheckPermission(self):
+        if self.gid not in self.NotAllowGroup:
+            return -1
+
     def GETMSG(self):
         if self.parms:
             return '咱也不知道' + self.parms[0] + '是啥呢~'
         else:
-            return '汝再调戏咱，咱可就生气了！！' 
+            return '汝再调戏咱，咱可就生气了！！'
