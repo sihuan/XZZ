2bba704dd8c45e65bf3c2946e234bebb4fed86f7
SiHuan <sihuan@sakuya.love>
2020-06-18 19:25:32 +0800

    update emmm

--- route.py
+++ route.py
@@ -1,23 +1,27 @@
 from worker import emmm
 
+
 def ZZRouter(data):
     uid = data['user_id']
     gid = data['group_id']
     role = data['sender']['role']
     parms = str.split(data['message'][1:])
-    print(uid,gid,role,parms)
+    print(uid, gid, role, parms)
     if parms == []:
         parms[0] = 'help'
     worker = parms[0]
 
     try:
-        package = __import__(name='worker.'+ worker, fromlist=worker)
-        Ans_ = getattr(package,'Ans')
+        package = __import__(name='worker.' + worker, fromlist=worker)
+        Ans_ = getattr(package, 'Ans')
     except:
         Ans_ = emmm.Ans
 
-    Ans = Ans_(parms,uid,gid,role,data)
+    Ans = Ans_(parms, uid, gid, role, data)
     Message = Ans.CheckPermission()
     if Message == 0:
         Message = Ans.GETMSG()
-    Ans.sendmsg(Message)
+    elif Message == -1:
+        pass
+    else:
+        Ans.sendmsg(Message)
