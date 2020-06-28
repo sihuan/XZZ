ab475b483ac9cb8e1b4aa107c80af10dc3809d88
SiHuan <sihuan@sakuya.love>
2020-06-28 12:38:59 +0800

    add dots group pixiv

--- worker/pixiv.py
+++ worker/pixiv.py
@@ -4,7 +4,7 @@ import requests
 from config import LOLIKEY
 
 class Ans(StdAns):
-    AllowGroup = [805197917,959613860,343700338,125733077,1084566280,920863253,798595664,655057127,196268763]
+    AllowGroup = [805197917,959613860,343700338,125733077,1084566280,920863253,798595664,655057127,196268763,474907856]
     def GETMSG(self):
         url = 'https://api.lolicon.app/setu/'
         params = {
