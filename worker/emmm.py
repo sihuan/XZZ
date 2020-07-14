27f3270f87afc7d5d7a9944b166c51baefa26c74
SiHuan <sihuan@sakuya.love>
2020-07-14 16:44:26 +0800

    only my sakuya

--- worker/emmm.py
+++ worker/emmm.py
@@ -11,6 +11,10 @@ class Ans(StdAns):
 
     def GETMSG(self):
         if self.parms:
+            if self.uid != 1318000868:
+                for sakuya in ['十六夜咲夜','十六夜','十六','咲夜','Sakuya','sakuya','Izayoi Sakuya','Izayoi','izayoi','izayoi sakuya']:
+                    if sakuya in self.parms[0]:
+                        return "咲夜是最完美的！！"
             return '咱也不知道' + self.parms[0] + '是啥呢~'
         else:
             return '汝再调戏咱，咱可就生气了！！'
