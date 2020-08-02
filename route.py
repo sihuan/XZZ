21ac9da11b5932033c707cefb5673fc4192e1d7b
SiHuan <sihuan@sakuya.love>
2020-08-02 23:13:31 +0800

    update zzrouter

--- route.py
+++ route.py
@@ -2,6 +2,9 @@ from worker import emmm
 
 
 def ZZRouter(data):
+    if data['message'][0] != '/':
+        return
+        
     uid = data['user_id']
     gid = data['group_id']
     role = data['sender']['role']
