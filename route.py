b89fef264e2e688ec291adcf5deb726153193c02
SiHuan <sihuan@sakuya.love>
2020-08-11 10:24:28 +0800

    回滚 router ，提升稳定性

--- route.py
+++ route.py
@@ -1,27 +1,8 @@
 from worker import emmm
-from zzcore import DM
-from config import DAAPI
-
-import requests
 
 
 def ZZRouter(data):
-
     if data['post_type'] != 'message' or data['message_type'] != 'group' or data['message'][0] != '/':
-        if data['user_id'] == 1318000868 :
-            return
-        if len(data['raw_message']) == 54 and data['raw_message'][:15] == '[CQ:image,file=':   # use re
-            url = data['message'][58:][:-1]
-            r = requests.post(
-                "https://api.deepai.org/api/image-similarity",
-                data={
-                    'image1': 'https://img.vim-cn.com/fa/4a7f3996e3601e98bd3c1b245fcb88e05f32ec.jpg',
-                    'image2': url,
-                },
-                headers={'api-key': DAAPI}
-            )
-            if r.json()['output']['distance'] < 10:
-                DM(data['message_id'])
         return
 
     uid = data['user_id']
