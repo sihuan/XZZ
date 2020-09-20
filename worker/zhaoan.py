08978a189067c0d171b9d6771fbdfd6e44c06b80
SiHuan <49349784+Si-Huan@users.noreply.github.com>
2020-09-20 23:24:48 +0800

    Update zhaoan.py

--- worker/zhaoan.py
+++ worker/zhaoan.py
@@ -9,8 +9,8 @@ class Ans(StdAns):
     def GETMSG(self):
         msg = f'早上好，今天是{calendar()}\n\n'
         msg += getWeather() + '\n\n'
-        # t = requests.get('https://v1.hitokoto.cn/?c=k&encode=text').text
-        t = ("预祝大家四六级考试顺利。")
+        t = requests.get('https://v1.hitokoto.cn/?c=k&encode=text').text
+        # t = ("预祝大家四六级考试顺利。")
         msg += t
         return msg
 
