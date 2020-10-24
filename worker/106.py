ce3ee93910661b5ba18047cdee16a58fedb9b012
SiHuan <sihuan@sakuya.love>
2020-10-24 18:43:39 +0800

    add 106 door

---
+++ worker/106.py
@@ -0,0 +1,10 @@
+import requests
+from zzcore import StdAns
+
+class Ans(StdAns):
+    AllowGroup = [959613860]
+    def GETMSG(self):
+        seconds = int(requests.get("http://127.0.0.1:8095/").text)
+        m, s = divmod(seconds, 60)
+        h, m = divmod(m, 60)
+        return f'{h}小时{m}分钟{s}秒前有人来过。'
