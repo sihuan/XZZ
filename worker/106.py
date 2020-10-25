dfe115dfb477a960eaee6b134bd8c5b5c902e668
MocyRee <50027802+MocyRee@users.noreply.github.com>
2020-10-25 19:04:17 +0800

    Update 106.py

--- worker/106.py
+++ worker/106.py
@@ -2,7 +2,7 @@ import requests
 from zzcore import StdAns
 
 class Ans(StdAns):
-    AllowGroup = [959613860]
+    AllowGroup = [959613860, 983250332]
     def GETMSG(self):
         seconds = int(requests.get("http://127.0.0.1:8095/").text)
         m, s = divmod(seconds, 60)
