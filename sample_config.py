b7b0c8c37c7406af35599f721a0da1c2ea46171c
SiHuan <sihuan@sakuya.love>
2021-08-12 19:19:03 +0800

    update

--- sample_config.py
+++ sample_config.py
@@ -1,3 +1,6 @@
+from config import BINGKEY
+
+
 ALLWORKERS = ['ping','yiyan','mc',]
 
 APIURL = 'http://127.0.0.1:5700/'
@@ -9,3 +12,5 @@ HOST = '0.0.0.0'
 PORT = 5580
 
 REMOTE_MC_URL = ''
+
+BINGKEY = ''
