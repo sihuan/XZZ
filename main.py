f5d10a3d88958cc9839aa1b385ef6293755166e1
SiHuan <sihuan@sakuya.love>
2022-04-21 16:39:49 +0800

    修复多次回复的问题

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
 from config import HOST, PORT
 from route import ZZRouter
+import threading
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def whenmsg():
-    ZZRouter(request.get_json())
+    threading.Thread(target=ZZRouter, args=(request.get_json(),),daemon=True).start()
+    # ZZRouter(request.get_json())
     return ''
 
 app.run(host= HOST,port = PORT, debug=True)
